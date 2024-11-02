ae9ce00a2621b30dade2dc15cce47da68f5946b8
Jiho Kil <jihokil@g.harvard.edu>
2024-11-02 13:52:12 -0400

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,8 +4,8 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="cheese-app-data-versioning"
-export GCP_PROJECT="ac215-project"
+export GCS_BUCKET_NAME="hw-cheese-app-data-versioning"
+export GCP_PROJECT="assignment-2-440516"
 export GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS="/secrets/data-service-account.json"
 
